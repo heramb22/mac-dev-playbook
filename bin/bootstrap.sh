ce5ec0c208d5e1f175d5b3f04035b79dd79deea4
daniel <daniel@heramb.net>
2016-09-12 14:30:06 -0400

    Adding a check for Xcode Command Line Tools before downloading and installing.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -13,14 +13,20 @@
 
 # install Xcode Command Line Tools
 # https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
-echo 'Installing Xcode Command Line Tools...'
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" -v;
+echo 'Checking for Xcode Command Line Tools...'
+if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+   test -d "${xpath}" && test -x "${xpath}" ; then
+    echo 'installing.'
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+    PROD=$(softwareupdate -l |
+      grep "\*.*Command Line" |
+      head -n 1 | awk -F"*" '{print $2}' |
+      sed -e 's/^ *//' |
+      tr -d '\n')
+    softwareupdate -i "$PROD" -v;
+else
+    echo 'already installed.'
+fi
 
 
 # We need at least ansible 2.0 for blockinfile directive
